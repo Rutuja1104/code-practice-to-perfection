7bc8fdb44c3df2394a2239cf51654828ae41f3aa
Rutuja1104 <rutujasahare2018@gmail.com>
2026-03-23 08:05:49 +0530

    changes for sum problems

---
+++ leetCode/AddTwoNumbers.js
@@ -0,0 +1,32 @@
+/**
+ * Input: l1 = [2,4,3], l2 = [5,6,4]
+Output: [7,0,8]
+Explanation: 342 + 465 = 807.
+ */
+
+function ListNode(val, next = null) {
+    this.val = val;
+    this.next = next;
+}
+
+var addTwoNumbers = function (l1, l2) {
+    let dummy = new ListNode(0);
+    let current = dummy;
+    let carry = 0;
+
+    while (l1 !== null || l2 !== null || carry !== 0) {
+        let val1 = l1 ? l1.val : 0;
+        let val2 = l2 ? l2.val : 0;
+
+        let sum = val1 + val2 + carry;
+        carry = Math.floor(sum / 10);
+
+        current.next = new ListNode(sum % 10);
+        current = current.next;
+
+        if (l1) l1 = l1.next;
+        if (l2) l2 = l2.next;
+    }
+
+    return dummy.next;
+};
---
+++ leetCode/TwoSum.js
@@ -0,0 +1,18 @@
+/**
+ * Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Explanation: Because nums[0] + nums[1] == 9, we return [0, 1]
+ */
+var twoSum = function (nums, target) {
+    const map = new Map();
+
+    for (let i = 0; i < nums.length; i++) {
+        let complement = target - nums[i];
+
+        if (map.has(complement)) {
+            return [map.get(complement), i];
+        }
+
+        map.set(nums[i], i);
+    }
+};
