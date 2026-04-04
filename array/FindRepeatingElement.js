a95f8a57146e64180bfdeb41f7354ead44e26484
Rutuja1104 <rutujasahare2018@gmail.com>
2026-04-04 14:16:51 +0530

    array

--- array/FindRepeatingElement.js
+++ array/FindRepeatingElement.js
@@ -1,7 +1,24 @@
 function findRepeatingElement(arr) {
-    let repeating = arr.filter((value, index, self) => {
-        return self.indexOf(value) !== self.lastIndexOf(value)
-    });
-    return new Set([...repeating]);
+    return [...new Set(arr.filter((item, i) => arr.indexOf(item) !== i))]
 }
 console.log(findRepeatingElement([1, 1, 2, 3, 4, 4, 5]))
+
+function findRepeatingElements(arr) {
+    const freq = {};
+    const result = [];
+
+    for (let num of arr) {
+        freq[num] = (freq[num] || 0) + 1;
+    }
+
+    for (let key in freq) {
+        if (freq[key] > 1) {
+            result.push(Number(key)); // convert key back to number
+        }
+    }
+
+    return result;
+}
+
+// Example
+console.log(findRepeatingElements([4, 5, 1, 2, 0, 4, 1, 2])); // [4, 1, 2]
