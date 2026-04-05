82528dd63fa0262a1e9dbe78d8d702b6ad1b1702
Rutuja1104 <rutujasahare2018@gmail.com>
2026-04-05 21:00:26 +0530

    String

---
+++ String/CountCharacters.js
@@ -0,0 +1,11 @@
+function countChars(str) {
+    let count = {};
+
+    for (let char of str) {
+        count[char] = (count[char] || 0) + 1;
+    }
+
+    return count;
+}
+
+console.log(countChars("hello"));
